f4242bbcdd58a1066c68c2d64fca939bc576629e
Jim Wang <turanuk@gmail.com>
2012-05-14 14:43:30 -0700

    Partial checkin

--- Admin/Endpoints/AddToParty.vbhtml
+++ Admin/Endpoints/AddToParty.vbhtml
@@ -6,7 +6,10 @@
     Response.SetStatus(500)
   Else
     Try
-      db.Execute("DELETE FROM RSVP WHERE InvitationId = @0", invitationCode)
+      Dim guest = db.QuerySingle("SELECT * FROM RSVP WHERE InvitationId = @0", invitationCode)
+      Dim count = guest.NumberInParty
+      count = count + 1
+      db.Execute("UPDATE RSVP SET NumberInParty = @0 WHERE InvitationId = @1", count, invitationCode)
     Catch ex As Exception
       Response.SetStatus(500)
     End Try
--- Knockout/ManageGuestsViewModel.js
+++ Knockout/ManageGuestsViewModel.js
@@ -1,20 +1,25 @@
 /// <reference path='../Scripts/knockout-2.1.0.js'/>
-var initialData = [{'Id':1,'InvitationId':957167,'LastName':'Wang','NumberInParty':2,'FirstName':'Jim','Responded':false},{'Id':2,'InvitationId':186143,'LastName':'Wang2','NumberInParty':2,'FirstName':'Jim','Responded':false}];
 
-var ManageGuestsViewModel = function (guestInput) {
+var ManageGuestsViewModel = function(guestInput) {
   var self = this;
   self.guests = ko.observableArray(guestInput);
-  self.columns = [ ];
-  self.sortByLastName = function () {
-    this.guests.sort(function (a,b) {
+  self.columns = [];
+  self.sortByLastName = function() {
+    this.guests.sort(function(a, b) {
       return a.LastName < b.LastName ? -1 : 1;
     });
   }
 
-  self.deleteGuest = function (guest, event, viewmodel) {
+  self.deleteGuest = function(guest) {
     $.post('/admin/endpoints/delete', { InvitationId: guest.InvitationId }, function(data) {
-      self.guests.remove(function (item) { return item.InvitationId ==  guest.InvitationId});
-    })
+      self.guests.remove(function(item) { return item.InvitationId == guest.InvitationId });
+    });
+  }
+
+  self.addToParty = function(guest) {
+    $.post('/admin/endpoints/addtoparty', { InvitationId: guest.InvitationId }, function(data) {
+      
+    });
   }
 }
 
