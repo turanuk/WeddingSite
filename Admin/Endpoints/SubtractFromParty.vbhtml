0bcb6cc7bd79d738fa30f34c53ef2882ccaada95
unknown <jimwa@WEBNETTS-V01.redmond.corp.microsoft.com>
2012-05-14 15:50:50 -0700

    Converting to spaces. Also adding SubtractFromParty endpoint

---
+++ Admin/Endpoints/SubtractFromParty.vbhtml
@@ -0,0 +1,17 @@
+﻿@Code
+  Layout = Nothing
+  Dim invitationCode = Request("InvitationId").AsInt()
+  Dim db = Database.Open("WeddingSite")
+  If invitationCode = Nothing
+    Response.SetStatus(500)
+  Else
+    Try
+      Dim guest = db.QuerySingle("SELECT * FROM RSVP WHERE InvitationId = @0", invitationCode)
+      Dim count = guest.NumberInParty
+      count = count - 1
+      db.Execute("UPDATE RSVP SET NumberInParty = @0 WHERE InvitationId = @1", count, invitationCode)
+    Catch ex As Exception
+      Response.SetStatus(500)
+    End Try
+  End If
+End Code
--- Knockout/ManageGuestsViewModel.js
+++ Knockout/ManageGuestsViewModel.js
@@ -1,30 +1,30 @@
 /// <reference path='../Scripts/knockout-2.1.0.js'/>
 
-var ManageGuestsViewModel = function(guestInput) {
+var ManageGuestsViewModel = function (guestInput) {
   var self = this;
   self.guests = ko.observableArray(guestInput);
   self.columns = [];
-  self.sortByLastName = function() {
-    this.guests.sort(function(a, b) {
+  self.sortByLastName = function () {
+    this.guests.sort(function (a, b) {
       return a.LastName < b.LastName ? -1 : 1;
     });
   }
 
-  self.deleteGuest = function(guest) {
-    $.post('/admin/endpoints/delete', { InvitationId: guest.InvitationId }, function(data) {
-      self.guests.remove(function(item) { return item.InvitationId == guest.InvitationId });
+  self.deleteGuest = function (guest) {
+    $.post('/admin/endpoints/delete', { InvitationId: guest.InvitationId }, function (data) {
+      self.guests.remove(function (item) { return item.InvitationId == guest.InvitationId });
     });
   }
 
-  self.addToParty = function(guest) {
-    $.post('/admin/endpoints/addtoparty', { InvitationId: guest.InvitationId }, function(data) {
-      
+  self.addToParty = function (guest) {
+    $.post('/admin/endpoints/addtoparty', { InvitationId: guest.InvitationId }, function (data) {
+      //alert('hi');
     });
   }
 }
 
-$(function() {
-  $.getJSON('/admin/endpoints/guests', function(data) {
+$(function () {
+  $.getJSON('/admin/endpoints/guests', function (data) {
     ko.applyBindings(new ManageGuestsViewModel(data));
   });
 });
